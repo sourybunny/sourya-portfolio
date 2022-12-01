7c5c995a178181e24a62642d254ee359a70b28ed
Varanasi, Sourya Suguna <sovarana@iu.edu>
2022-12-01 11:10:56 +0530

    ref: contact

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,14 +1,8 @@
 <template>
-  <div class="black">
-    <v-lazy
-      v-model="isActive"
-      :options="{
-        threshold: 0.6,
-      }"
-      transition="fade-transition"
-    >
-      <v-container
-        class="  white--text py-16 px-10 center-align"
+  <div class="mainbg">
+    <div class="layout-container">
+      <div
+        class="  white--text py-16 px-10 center-align layout-container__inner"
         width="100%"
         :class="$vuetify.breakpoint.mdAndUp ? 'display-2' : 'display-1'"
       >
@@ -21,8 +15,52 @@
             >
           </p>
         </div>
-      </v-container>
-    </v-lazy>
+      </div>
+    </div>
+    <div class="layout-container " style="border-top: 2px solid rgba(255, 255, 255, 0.1);">
+      <div
+        class="layout-container__inner py-6 white--text d-flex align-center justify-space-between"
+      >
+        <div>
+          © Sourya Varanasi
+        </div>
+        <div class="d-flex grey--text">
+          <v-btn
+            @click.stop="$router.push({ path: '/about' })"
+            plain
+            dark
+            class="text-capitalize"
+            >Bio</v-btn
+          >
+          <v-btn
+            href="https://www.linkedin.com/in/souryav/"
+            target="_blank"
+            plain
+            dark
+            class="text-capitalize"
+            >Linkedin</v-btn
+          >
+          <v-btn
+            href="https://www.instagram.com/sourya_sketches/"
+            target="_blank"
+            plain
+            dark
+            class="text-capitalize"
+            >Instagram</v-btn
+          >
+          <v-btn
+            href="https://www.behance.net/sourya"
+            target="_blank"
+            plain
+            dark
+            class="text-capitalize"
+            >Behance</v-btn
+          >
+          <v-btn  href="https://www.github.com/sourybunny"
+            target="_blank" plain dark class="text-capitalize">Github</v-btn>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/components/header.vue
+++ src/components/header.vue
@@ -2,13 +2,30 @@
   <div class="layout-container navbar">
     <div class="layout-container__inner px-10 py-5 ">
       <div class="d-flex white--text align-center justify-space-between">
-        
-        <div @click.stop="$router.push({ path: '/' })" class="d-flex align-center is-pointer"><span><Logo class="mx-4" /></span><span style="font-family:'Sacramento',cursive; font-size:1.8rem;">Sv</span></div>
+        <div
+          @click.stop="$router.push({ path: '/' })"
+          class="d-flex align-center is-pointer"
+        >
+          <span><Logo class="mx-4 mr-6"/></span
+          ><span style="font-family:'Homemade apple',cursive; font-size:1.4rem;"
+            >Sv</span
+          >
+        </div>
         <div class="d-flex align-center">
-          <v-btn plain dark class="text-capitalize" @click.stop="$router.push({ path: '/' })">
+          <v-btn
+            plain
+            dark
+            class="text-capitalize"
+            @click.stop="$router.push({ path: '/' })"
+          >
             Home
           </v-btn>
-          <v-btn plain dark class="text-capitalize" @click.stop="$router.push({ path: '/about' })">
+          <v-btn
+            plain
+            dark
+            class="text-capitalize"
+            @click.stop="$router.push({ path: '/about' })"
+          >
             About
           </v-btn>
         </div>
@@ -18,11 +35,11 @@
 </template>
 
 <script>
-import Logo from './logo.vue';
+import Logo from "./logo.vue";
 export default {
-  components:{
-    Logo
-  }
+  components: {
+    Logo,
+  },
 };
 </script>
 
